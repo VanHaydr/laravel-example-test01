53d9194aac68a0129bd74673403368aaf1b4b1e3
Ivan212-INH <nabiljapar@gmail.com>
2025-09-19 06:07:41 +0700

    add csrf

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,5 @@
 <form method="POST" action=" {{ route('login.attempt') }} ">
+    @csrf
     <input type="email" placeholder="Email" required />
     <input type="password" placeholder="Password" required/>
     <button type="submit">Submit</button>
