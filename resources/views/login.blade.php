56c03995ac39b85cb9f0e4d778f3b21622a369a0
Ivan212-INH <nabiljapar@gmail.com>
2025-09-19 07:27:32 +0700

    add errors catch

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,15 @@
-<form method="POST" action=" {{ route('login') }} ">
+<form method="POST" action=" {{ route('login.attempt') }} ">
     @csrf
+    @if ($errors->any())
+     <div>
+        <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul>
+    </div>
+        
+    @endif
     <input type="email" name="email" placeholder="Email" required />
     <input type="password" name="password" placeholder="Password" required/>
     <button type="submit">Submit</button>
