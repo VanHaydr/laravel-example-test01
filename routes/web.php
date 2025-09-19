ca057078f807f1612bad547e9b4e33b62d9706c3
Ivan212-INH <nabiljapar@gmail.com>
2025-09-19 07:29:27 +0700

    add name for home

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Auth\LoginController;
 
 
 Route::get('/', function () {return view('welcome');});
-Route::get('/home', function () {return view('home');});
+Route::get('/home', function () {return view('home');})->name('home');
 
 //login
 Route::get('/login', function() {return view('login');})->name('login');
