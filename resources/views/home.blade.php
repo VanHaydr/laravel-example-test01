3bc5f7fc2d4a8e7df7381500216b77b41c904953
Ivan212-INH <nabiljapar@gmail.com>
2025-09-19 07:49:05 +0700

    add logout button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>
-        test
-    </title>
-
-</head>
-<body>
 <h1>hello world</h1>
 <h1>hello {{ Auth::user()->name }}</h1>
-</body>
-</html>
+<form method="POST" action="{{ route('logout') }}">
+    @csrf
+    <button type="submit">Logout</button>
+</form>
